a08010d58dad4abcf28da66bb5aa5fbce3754013
stormie8 <tehjohnli888@gmail.com>
2024-08-22 20:10:11 -0400

    Added CORS headers

--- service/__init__.py
+++ service/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_talisman import Talisman
 from service import config
 from service.common import log_handlers
+from flask_cors import CORS
 
 # Create Flask application
 app = Flask(__name__)
@@ -9,6 +10,9 @@ app.config.from_object(config)
 
 # Initialize Talisman
 talisman = Talisman(app)
+
+# Initialize CORS
+CORS(app)
 
 # Set up logging for production
 log_handlers.init_logging(app, "gunicorn.error")
